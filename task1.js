a4d1b0cc2bfe93717d5253a7f5608a281a294d87
DeadlyVlad <153931394+DeadlyVlad@users.noreply.github.com>
2024-11-12 16:59:59 +0200

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,33 @@
+"Ваша функція повинна робити GET-запит до вказаного URL і отримати дані."
+"Поверніть дані користувачів у форматі масиву"
+"Дані мають включати такі поля, як id та name."
+
+"https://jsonplaceholder.typicode.com/users - адреса куди робити запит"
+
+function fetchUsers() {
+  const https = require('https');
+  return new Promise((resolve, reject) => {
+    https.get('https://jsonplaceholder.typicode.com/users', (res) => {
+      let data = '';
+
+      res.on('data', (chunk) => {
+        data += chunk;
+      });
+
+      res.on('end', () => {
+        try {
+          const users = JSON.parse(data).map(user => ({ id: user.id, name: user.name }));
+          resolve(users);
+        } catch (error) {
+          reject(error);
+        }
+      });
+    }).on('error', (error) => {
+      reject(error);
+    });
+  });
+}
+
+console.log(fetchUsers())
+
+module.exports = fetchUsers;
---
+++ task2.js
@@ -0,0 +1,37 @@
+"Ваш код повинен зробити POST-запит до вказаного URL."
+"Для створення нового користувача, передайте в запит наступні дані:"
+"name: ваше ім’я"
+"email: ваш email"
+"Поверніть відповідь від сервера після створення користувача."
+
+"https://jsonplaceholder.typicode.com/users - адреса куди робити запит"
+
+
+async function createUser(user) {
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error creating user:', error);
+    return null;
+  }
+}
+
+createUser({ name: "Sam", email: "fjsnfkjns2342@gmail.com" })
+  .then(response => console.log(response)); 
+
+console.log(createUser({name: "Sam", email: "fjsnfkjns2342@gmail.com"}))
+
+module.exports = createUser;
